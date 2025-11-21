08aa2074a98c72f1ede224321cd502a91d468357
Wendy Yepez <wendypaolayepezaguayo2901@.com>
2025-11-20 19:29:26 -0500

    Se agrego el Módulo de Gestión de Investigadores

--- src/lib/sidebarItems.tsx
+++ src/lib/sidebarItems.tsx
@@ -2,6 +2,7 @@ export const sidebarItems: any = {
   admin: [
     { name: "Gestión de Usuarios", href: "/admin/users", icon: "👨🏻‍💻" },
     { name: "Autorización de Estaciones", href: "/admin/stations", icon: "🖥️" },
+    { name: "Módulo de Gestión de Investigadores", href: "/admin/stations/researcher", icon: "👩‍🔬" },
   ],
   estacion: [
     { name: "Solicitudes", href: "/estacion/solicitudes", icon: "📩" },
@@ -11,10 +12,12 @@ export const sidebarItems: any = {
     { name: "Investigadores", href: "/institucion/investigadores", icon: "👩‍🔬" },
     { name: "Permisos", href: "/institucion/permisos", icon: "📝" },
   ],
-  investigador: [
-    { name: "Datos", href: "/investigador/datos", icon: "📊" },
-    { name: "Reportes", href: "/investigador/reportes", icon: "📈" },
-  ],
+investigador: [
+  { name: "Mi Perfil", href: "/researcher/perfil", icon: "👤" },
+  { name: "Gestión de Mis Estaciones", href: "/dashboard/researcher", icon: "📡" }, 
+  { name: "Generación de Reportes", href: "/researcher/reportes", icon: "📄" },
+  { name: "Tendencias, Alertas y Mantenimiento", href: "/researcher/tendencias", icon: "📊" }
+],
   ciudadano: [
     { name: "Información Pública", href: "/ciudadano/info", icon: "🌍" },
     { name: "Mapa de Estaciones", href: "/ciudadano/mapa", icon: "📍" },
