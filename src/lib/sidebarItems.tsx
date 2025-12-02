c2e169b5f7999a3325ceb8febebbe0317e6d6823
Wendy Yepez <100549599+Ween19@users.noreply.github.com>
2025-12-01 20:38:25 -0500

    Update sidebarItems.tsx

--- src/lib/sidebarItems.tsx
+++ src/lib/sidebarItems.tsx
@@ -2,6 +2,9 @@ export const sidebarItems: any = {
   admin: [
     { name: "Gestión de Usuarios", href: "/admin/users", icon: "👨🏻‍💻" },
     { name: "Autorización de Estaciones", href: "/admin/stations", icon: "🖥️" },
+    { name: "Gestión de Investigadores", href: "/admin/stations/researcher", icon: "👩‍🔬" },
+    // solo lo agrego para que lo vean 1 vez ya que da erro por que es diferente direccion  si les da error comentenlo
+    { name: "Mi Perfil", href: "/dashboard/researcher", icon: "👤" }, 
   ],
   estacion: [
     { name: "Solicitudes", href: "/estacion/solicitudes", icon: "📩" },
@@ -11,9 +14,12 @@ export const sidebarItems: any = {
     { name: "Investigadores", href: "/institucion/investigadores", icon: "👩‍🔬" },
     { name: "Permisos", href: "/institucion/permisos", icon: "📝" },
   ],
+
   investigador: [
-    { name: "Datos", href: "/investigador/datos", icon: "📊" },
-    { name: "Reportes", href: "/investigador/reportes", icon: "📈" },
+  { name: "Mi Perfil", href: "/dashboard/researcher", icon: "👤" },
+  { name: "Gestión de Mis Estaciones", href: "/dashboard/researcher/stations", icon: "📡" },
+  { name: "Generación de Reportes", href: "/dashboard/researcher/reportes", icon: "📄" },
+  { name: "Tendencias, Alertas y Mantenimiento", href: "/dashboard/researcher/trends", icon: "📊" }
   ],
   ciudadano: [
     { name: "Información Pública", href: "/ciudadano/info", icon: "🌍" },
